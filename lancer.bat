084c24716b565bfbf383cf418b692bbdb9c53d35
gwenlbt <glebreton001@ensc.fr>
2026-04-22 17:39:40 +0200

    lancer.bat

--- lancer.bat
+++ lancer.bat
@@ -1,11 +1,13 @@
 @echo off
 echo Démarrage de PLANOGEC...
 cd /d "%~dp0"
-start "Serveur PLANOGEC" cmd /k "cd backend && node server.js"
+
+start "Serveur PLANOGEC" cmd /k "cd /d "%~dp0backend" && node server.js"
 timeout /t 2 /nobreak >nul
-cd ogec-dashboard
-start "Interface PLANOGEC" cmd /k "npm start"
+
+start "Interface PLANOGEC" cmd /k "cd /d "%~dp0ogec-dashboard" && npm start"
 timeout /t 5 /nobreak >nul
+
 start http://localhost:3000
 echo PLANOGEC est lancé ! Vous pouvez fermer cette fenêtre.
 pause
